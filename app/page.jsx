471c2f9b21e459028bbfbd712820fd80f793727c
markhadi <markhadi@protonmail.com>
2023-09-24 20:46:50 +0800

    menu moble toggle

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,20 @@
-import Nav from "@/components/Nav";
-
 export default function Home() {
   return (
-    <>
-      <Nav />
-      <main className="z-[1] pb-12 sm:pb-[90px] lg:pb-[132px]">
-        <div className="flex flex-col gap-[81px] items-center px-6 sm:gap-[156px] lg:flex-row lg:max-w-[1110px] lg:px-16 lg:justify-between lg:items-end lg:mx-auto xl:px-0">
-          <div className="flex flex-col gap-4 text-center max-w-[445px] sm:gap-6 lg:text-left">
-            <span className="nav_text">SO, YOU WANT TO TRAVEL TO</span>
-            <h1>SPACE</h1>
-            <p>
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
-
-          <button className="btn_circle">explore</button>
+    <main className="pb-12 sm:pb-[90px] lg:pb-[132px]">
+      <div className="flex flex-col gap-[81px] items-center px-6 sm:gap-[156px] lg:flex-row lg:max-w-[1110px] lg:px-16 lg:justify-between lg:items-end lg:mx-auto xl:px-0">
+        <div className="flex flex-col gap-4 text-center max-w-[445px] sm:gap-6 lg:text-left">
+          <span className="nav_text">SO, YOU WANT TO TRAVEL TO</span>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
         </div>
-      </main>
-    </>
+
+        <button className="btn_circle">explore</button>
+      </div>
+    </main>
   );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,19 +2,22 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const menuArray = [
-  { id: "01", name: "home" },
-  { id: "02", name: "destination" },
-  { id: "03", name: "crew" },
-  { id: "04", name: "technology" },
+  { id: "00", name: "home" },
+  { id: "01", name: "destination" },
+  { id: "02", name: "crew" },
+  { id: "03", name: "technology" },
 ];
 
 const Nav = () => {
   const pathname = usePathname();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="z-[1] flex justify-between items-center px-6 pt-6 mb-12 sm:pt-0 sm:px-0 sm:pl-10 sm:mb-[106px] lg:pl-[55px] lg:pt-10 lg:mb-[250px]">
+    <nav className="flex justify-between items-center px-6 pt-6 mb-12 sm:pt-0 sm:px-0 sm:pl-10 sm:mb-[106px] lg:pl-[55px] lg:pt-10 lg:mb-[250px]">
       <Link href="/">
         <img
           src="/assets/shared/logo.svg"
@@ -22,12 +25,26 @@ const Nav = () => {
           className="w-10 h-10 sm:w-12 sm:h-12"
         />
       </Link>
-      <button className="sm:hidden">
-        <img src="/assets/shared/icon-hamburger.svg" alt="icon hamburger" />
+
+      <button
+        className="grid place-content-center z-10 sm:hidden"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <img
+          src="/assets/shared/icon-hamburger.svg"
+          alt="icon hamburger"
+          className={`icon_transition ${isMenuOpen ? "" : "icon_visible"}`}
+        />
+        <img
+          src="/assets/shared/icon-close.svg"
+          alt="icon close"
+          className={`icon_transition ${isMenuOpen ? "icon_visible" : ""}`}
+        />
       </button>
 
-      <div className="hidden sm:flex sm:px-12 sm:py-[38px] sm:bg-[rgba(255,255,255,0.04)] sm:backdrop-blur-[40px] lg:px-0 lg:pl-[120px] lg:pr-[168px]">
-        <div className="flex gap-7 text-white font-barlow-condensed text-[16px] leading-[20px] tracking-[2.7px] uppercase">
+      {/* menu desktop */}
+      <div className="hidden menu_desktop">
+        <div className="flex gap-7 menu_item">
           {menuArray.map((item) => {
             const itemPath =
               item.name.toLowerCase() === "home"
@@ -38,7 +55,7 @@ const Nav = () => {
 
             return (
               <div key={item.id} className="relative flex gap-3">
-                <span className="hidden lg:block">{item.id}</span>
+                <span className="hidden font-bold lg:block">{item.id}</span>
                 <Link
                   href={itemPath}
                   className={`menu_hover ${isActive ? "menu_active" : ""}`}
@@ -48,6 +65,36 @@ const Nav = () => {
               </div>
             );
           })}
+        </div>
+      </div>
+
+      {/* menu mobile */}
+      <div className={`menu_mobile ${isMenuOpen ? "" : "hide_menu"} sm:hidden`}>
+        <div className="flex flex-col gap-8 menu_item">
+          {isMenuOpen
+            ? menuArray.map((item) => {
+                const itemPath =
+                  item.name.toLowerCase() === "home"
+                    ? "/"
+                    : `/${item.name.toLowerCase()}`;
+
+                const isActive = pathname === itemPath;
+
+                return (
+                  <div key={item.id} className="relative flex gap-3">
+                    <span className="font-bold">{item.id}</span>
+                    <Link
+                      href={itemPath}
+                      className={`menu_hover_mobile ${
+                        isActive ? "menu_active_mobile" : ""
+                      }`}
+                    >
+                      {item.name}
+                    </Link>
+                  </div>
+                );
+              })
+            : ""}
         </div>
       </div>
     </nav>
