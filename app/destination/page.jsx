6f545665f68763ff348b689155ff51f71fe6a57b
markhadi <markhadi@protonmail.com>
2023-09-26 04:47:57 +0800

    /destination style tablet

--- app/destination/page.jsx
+++ app/destination/page.jsx
@@ -38,27 +38,30 @@ const DestinationPage = () => {
     >
       <Nav />
 
-      <main className="mt-6 pb-14">
-        <div className="flex flex-col items-center gap-8 px-6">
-          <h5 className="flex gap-5 text-white uppercase text-[16px] leading-[20px] tracking-[2.7px]">
-            <span className="font-bold text-[rgba(255,255,255,0.25)]">01</span>
+      <main className="mt-6 pb-14 sm:mt-10 sm:pb-16">
+        <div className="flex flex-col items-center gap-8 px-6 sm:gap-[60px] sm:px-0 ">
+          <h5 className="flex gap-5 text-white uppercase text-[16px] leading-[20px] tracking-[2.7px] sm:self-start sm:gap-7 sm:text-[20px] sm:leading-[24px] sm:tracking-[3.38px]">
+            <span className="font-bold text-[rgba(255,255,255,0.25)] sm:pl-10 ">
+              01
+            </span>
             Pick your destination
           </h5>
 
-          <div className="flex flex-col items-center gap-7 ">
+          <div className="flex flex-col items-center gap-7 sm:gap-14 ">
             {text.images ? (
               <Image
                 src={text.images}
                 width={170}
                 height={170}
                 alt={text.name}
+                className="sm:w-[300px] sm:h-[300px]"
               />
             ) : (
-              <Skeleton className="w-[170px] h-[170px]" />
+              <Skeleton className="w-[170px] h-[170px] sm:w-[300px] sm:h-[300px]" />
             )}
 
-            <div className="flex flex-col justify-center items-center">
-              <div className="text-blue-light flex gap-7 mb-5">
+            <div className="flex flex-col justify-center items-center sm:max-w-xl">
+              <div className="text-blue-light text-[14px] leading-[17px] tracking-[2.36px] flex gap-7 mb-9 sm:gap-9 sm:text-[16px] sm:leading-[20px] sm:tracking-[2.7px] sm:mb-12">
                 {destinations.map((destination, index) => (
                   <button
                     key={destination.name}
@@ -74,7 +77,7 @@ const DestinationPage = () => {
                 ))}
               </div>
               {text.name ? (
-                <h2 className="text-white font-bellefair text-[56px] uppercase">
+                <h2 className="text-white font-bellefair text-[56px] leading-[64px] uppercase sm:text-[80px] sm:leading-[92px] sm:mb-2">
                   {text.name}
                 </h2>
               ) : (
@@ -82,15 +85,16 @@ const DestinationPage = () => {
               )}
 
               {text.description ? (
-                <p className="text-blue-light text-center font-barlow text-[15px] leading-[25px]">
+                <p className="text-blue-light text-center font-barlow text-[15px] leading-[25px] sm:text-[16px] sm:leading-[28px]">
                   {text.description}
                 </p>
               ) : (
                 <Skeleton className="w-full h-20 mt-2" />
               )}
 
-              <hr className="w-full border border-[#383B4B] my-8" />
-              <div className="flex flex-col items-center gap-8">
+              <hr className="w-full border border-[#383B4B] my-8 sm:my-0 sm:mt-12 sm:mb-7" />
+
+              <div className="flex flex-col items-center gap-8 sm:flex-row sm:w-[350px] sm:justify-between">
                 <div className="flex flex-col gap-3 text-center">
                   <span className="uppercase text-blue-light text-[14px] leading-[17px] font-barlow-condensed tracking-[2.36px]">
                     AVG. DISTANCE
